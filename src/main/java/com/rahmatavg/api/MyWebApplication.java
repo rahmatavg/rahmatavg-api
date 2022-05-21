1a334c69ad0e0c2b1ca9a61376d9ac5d40ab7933
rahmatavg <rahmatavgweb@gmail.com>
2022-05-21 16:12:06 +0700

    rahmat
    perbaikan nama project dan port server ke default

--- src/main/java/com/rahmatavg/api/MyWebApplication.java
+++ src/main/java/com/rahmatavg/api/MyWebApplication.java
@@ -9,10 +9,10 @@ import java.util.Collections;
 public class MyWebApplication {
 
     public static void main(String[] args) {
-        // SpringApplication.run(MyWebApplication.class, args);
-        SpringApplication app = new SpringApplication(MyWebApplication.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", "5000"));
-        app.run(args);
+        SpringApplication.run(MyWebApplication.class, args);
+//        SpringApplication app = new SpringApplication(MyWebApplication.class);
+//        app.setDefaultProperties(Collections.singletonMap("server.port", "5000"));
+//        app.run(args);
     }
 
 }
